bd757e4db5e0bc16ac89e6e2192062135a0e2d27
Andrew Lelos <andrew.lelos@team.telstra.com>
2023-08-19 11:03:30 +1000

    add googlemaps sdk

---
+++ backend/googlemaps.helper.js
@@ -0,0 +1,3 @@
+import { Client } from '@googlemaps/google-maps-services-js';
+process.env.GOOGLE_MAPS_API_KEY = 'AIzaSyDgzgT_3aZJ_a1lWK_juF-Zn5x-Wy8nTB8';
+export default googlemaps = new Client({});

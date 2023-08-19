94c58c22fcd0cf2f2860668f2741d0b4cc90e42e
Paul Atkins <paul.atkins.2@team.telstra.com>
2023-08-19 11:51:32 +1000

    locations repo and fetch client

---
+++ frontend/data/repository/locationsData.js
@@ -0,0 +1,39 @@
+// an array of interesting locations around South Australia
+// a location has a name, description, coordinates, category and image
+export const locations = [
+  {
+    name: 'Church 1',
+    description: 'A church',
+    coordinates: { lat: -34.9285, lng: 138.6007 },
+    category: 'church',
+    image: 'church1.jpg'
+  },
+  {
+    name: 'Church 2',
+    description: 'A church',
+    coordinates: { lat: -34.9285, lng: 138.6007 },
+    category: 'church',
+    image: 'church2.jpg'
+  },
+  {
+    name: 'Museum 1',
+    description: 'A museum',
+    coordinates: { lat: -34.9285, lng: 138.6007 },
+    category: 'museum',
+    image: 'museum1.jpg'
+  },
+  {
+    name: 'Museum 2',
+    description: 'A museum',
+    coordinates: { lat: -34.9285, lng: 138.6007 },
+    category: 'museum',
+    image: 'museum2.jpg'
+  },
+  {
+    name: 'Park 1',
+    description: 'A park',
+    coordinates: { lat: -34.9285, lng: 138.6007 },
+    category: 'park',
+    image: 'park1.jpg'
+  }
+];

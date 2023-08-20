c8d8ae7d6d844bcf8989d64c075d83795af55bb7
Andrew Lelos <andrew.lelos@team.telstra.com>
2023-08-20 11:30:45 +1000

    remove google key

---
+++ backend/README.md
@@ -0,0 +1,3 @@
+Create a .env.local file with your Google Maps API key
+
+GOOGLE_MAPS_API_KEY=<API-KEY>
--- backend/googlemaps.helper.js
+++ backend/googlemaps.helper.js
@@ -1,5 +1,4 @@
 import { Client } from '@googlemaps/google-maps-services-js';
-process.env.GOOGLE_MAPS_API_KEY = 'AIzaSyDgzgT_3aZJ_a1lWK_juF-Zn5x-Wy8nTB8';
 const googlemaps = new Client({});
 
 /**
@@ -36,7 +35,6 @@ export async function getJourney({ start, destination, types }) {
           type,
           key: process.env.GOOGLE_MAPS_API_KEY,
           rankby: 'prominence',
-          keyword,
         },
       })
     );
